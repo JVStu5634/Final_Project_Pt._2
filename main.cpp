0c413705a83e2207c9ef02e8e8b90cb72d9a1aa2
JVStu5634 <valdezjoseph123@gmail.com>
2026-05-04 18:46:43 -0500

    Enhance program with student record management menu
    Updated the program to include a menu for managing student records.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
+// Joseph Valdez
+// 5/1/2026
+//  This is an updated version of the first program that takes daily quiz scores and displays the highest and lowest scores. 
+// This program now contains a menu that allows the user to choose whether to look at the data currently saved, add new student name,
+// search for students by name, delete a student's record and save new data added by the user.
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+const int MAX_SIZE = 100;
+
+struct Student {
+string name;
+int score;
+};
+
+
+
+
+
+
 

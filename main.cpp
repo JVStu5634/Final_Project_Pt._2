1411187377b1c5e682b397e858e5299db6d3cf1e
JVStu5634 <valdezjoseph123@gmail.com>
2026-05-04 21:53:53 -0500

    Update date in header comment
    Updated the date in the header comment to reflect the latest modification.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 // Joseph Valdez
-// 5/1/2026
+// 5/4/2026
 //  This is an updated version of the first program that takes daily quiz scores and displays the highest and lowest scores. 
 // This program now contains a menu that allows the user to choose whether to look at the data currently saved, add new student name,
 // search for students by name, delete a student's record and save new data added by the user.

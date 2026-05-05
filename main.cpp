ff5a1784e483fbc2b3b763e82c1a657ae3ba039b
JVStu5634 <valdezjoseph123@gmail.com>
2026-05-04 19:39:28 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,13 +13,118 @@ using namespace std;
 
 const int MAX_SIZE = 100;
 
-struct Student {
-string name;
+struct Student {      // Changed Class to Student 
+string name;       
 int score;
 };
 
+// Function Prototypes
+void displayData(Student students[], int count);
+void findMinMax(Student students[], int count);
+void addEntry(Student students[], int& count);
+void searchEntry(Student students[], int count);
+void saveData(Student students[], int count);
+void deleteEntry(Student students[], int& count);
 
+int main() {
 
+  Student students[MAX_SIZE];
+  int count = 0;
+  int choice;
+
+// initial load from data file
+
+ifstream inFile("Output.txt");
+if (inFile) {
+    while (count < MAX_SIZE && inFile >> students[count].name >> students[count].score) {
+        count++;
+    }
+
+    inFile.close();
+    cout << "Loaded" << count << " records from class file.\n";
+} else {
+    cout << "No existing file found. Please try again.\n";
+}
+
+do {
+    cout << "\n--- Daily Quiz Menu ---\n";
+    cout << "1. View Data\n";
+    cout << "2. Add Entry\n";
+    cout << "3. Search Entry\n";
+    cout << "4. Save Data\n";
+    cout << "5. Delete Data\n";
+    cout << "6. Exit\n";
+    cout << "Enter choice: ";
+    cin >> choice;
+
+    switch (choice) {
+        case 1:
+            displayData(students, count);
+            if (count > 0) findMinMax(students, count);
+            break;
+        case 2:
+            addEntry(students, count);
+            break;
+        case 3:
+            searchEntry(students, count);
+            break;
+        case 4:
+            saveData(students, count);
+            break;
+        case 5:
+            deleteEntry(students, count);
+            break;
+        case 6:
+            cout << "Exiting program... See you tomorrow!\n";
+            break;
+        default:
+            cout << "Invalid choice! Please try again!\n";
+    }
+  } while (choice != 6);
+
+  return 0;
+}
+
+void displayData(Student students[], int count) {
+  if (count == 0) {
+      cout << "List is empty.\n";
+      return;
+  }
+  cout << "\n" << left << setw(15) << "Name" << "Score" << endl;
+  cout << "----------------------\n";
+    for (int i = 0; i < count; i++) {
+        cout << left << setw(15) << students[i].name << students[i].score << endl;
+    }
+}
+
+void findMinMax(Student students[], int count) {
+  int maxIdx = 0, minIdx = 0;
+  for (int i = 0; i < count; i++) {
+      if (students[i].score > students[maxIdx].score)
+        maxIdx = i;
+      if (students[i].score < students[minIdx].score)
+        minIdx = i;
+   }
+    cout << "\nTodays Highest Quiz Score: " << students[maxIdx].name << " (" << students[maxIdx].score << ")";
+    cout << "\nTodays Lowest Quiz Score: " << students[minIdx].name << " (" << students[minIdx].score << ")";
+}
+
+void addEntry(Student students[], int& count) {
+  if (count < MAX_SIZE) {
+      cout << "Enter name: ";
+      cin >> students[count].name;
+      cout << "Enter score: ";
+      cin >> students[count].score;
+      count++;
+      cout << "Student added successfully. \n";
+  } else {
+      cout << "Classroom Full!\n";
+  }
+}
+
+void searchEntry(Student students[], int count) {
+  string query;
+  cout << "Enter name to 
 
 
 
